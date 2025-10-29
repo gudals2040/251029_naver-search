b42745a03329317adc7e810177cb075e2844c9e3
PARK HYEONG MIN <qd0420@gmail.com>
2025-10-29 16:18:37 +0900

    step03

--- src/Application.java
+++ src/Application.java
@@ -1,18 +1,26 @@
-import model.HttpException;
+import model.NewsItem;
 import service.NaverNewsSearchService;
 import service.NaverSearchService;
+import util.SearchUtil;
 
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         NaverSearchService searchService = new NaverNewsSearchService();
-        String result = searchService.search("칼국수");
-        System.out.println(result);
+        // TODO : NaverImageSearchService();
+        // 파싱 -> JSON 해석
+        String query = "칼국수";
+        String result = searchService.search(query);
+        if (searchService instanceof NaverNewsSearchService) {
+            String tmp = result.split("\"items\":\\[")[1].strip();
+            String[] items = tmp.split("},");
+            List<NewsItem> itemList = new ArrayList<>();
+            for (String v : items) {
+                itemList.add(SearchUtil.getNewsItem(v));
+            }
+            SearchUtil.saveNewsItem(query, itemList);
+        }
     }
 }
