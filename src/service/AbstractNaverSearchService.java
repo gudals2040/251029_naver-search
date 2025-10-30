23e5f53639414b62a82ae286f36b48b2da91c3e1
PARK HYEONG MIN <qd0420@gmail.com>
2025-10-30 09:23:27 +0900

    251030 / step 01

--- src/service/AbstractNaverSearchService.java
+++ src/service/AbstractNaverSearchService.java
@@ -2,12 +2,14 @@ package service;
 
 // 추상 클래스
 public abstract class AbstractNaverSearchService implements NaverSearchService, HttpClientService {
-    protected final String baseUrl = "https://openapi.naver.com/v1/search/news.json";
+    //    protected final String baseUrl = "https://openapi.naver.com/v1/search/news.json";
+    protected final String baseUrl; // 유동적으로 만들기 위해 baseUrl을 생성자로 넣는 방식으로 변경
     protected final String NAVER_CLIENT_ID = System.getenv("NAVER_CLIENT_ID");
     protected final String NAVER_CLIENT_SECRET = System.getenv("NAVER_CLIENT_SECRET");
     protected final String[] headers = {"X-Naver-Client-Id", NAVER_CLIENT_ID, "X-Naver-Client-Secret", NAVER_CLIENT_SECRET};
 
-    protected AbstractNaverSearchService() {
+    protected AbstractNaverSearchService(String baseUrl) {
+        this.baseUrl = baseUrl;
         // 설정을 안해서 null 값이 나왔을 때
         if (NAVER_CLIENT_ID == null || NAVER_CLIENT_SECRET == null)  {
             // 401가 예정되어 있는 셈
